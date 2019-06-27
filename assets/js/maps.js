4d14df0ac8b12e1b5dffd22f0714d541f8595111
Your full name <Your github email>
2019-06-27 10:20:47 +0000

    finishing my CV

---
+++ assets/js/maps.js
@@ -0,0 +1,25 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 51.4862492, lng: -0.0683309 },
+        { lat: 50.9590143, lng: -1.3758481 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
